9a2eac10b99142146bc421054c6a362a8b9066f6
zh6335901 <zh6335901@hotmail.com>
2022-04-03 17:00:31 +0800

    Add the comments of Bacthing options

--- src/CsKafka/Producing/Batching.cs
+++ src/CsKafka/Producing/Batching.cs
@@ -7,12 +7,29 @@
     /// </summary>
     public abstract record Batching
     {
+        /// <summary>
+        /// Produce individually, lingering for throughput + compression
+        /// </summary>
+        /// <param name="LingerMs"></param>
         public sealed record Linger(int LingerMs) : Batching;
 
+        /// <summary>
+        /// Use MaxInFlight = 1 to ensure messages in order
+        /// </summary>
+        /// <param name="LingerMs"></param>
         public sealed record EnsureOrder(int LingerMs) : Batching;
 
+        /// <summary>
+        /// Use EnableIdempotence = true and MaxInFlight = 5 to ensure messages in order
+        /// </summary>
+        /// <param name="LingerMs"></param>
         public sealed record EnsureOrderByEnableIdempotence(int LingerMs) : Batching;
 
+        /// <summary>
+        /// Apply custom-defined settings
+        /// </summary>
+        /// <param name="LingerMs"></param>
+        /// <param name="MaxInFlight"></param>
         public sealed record Custom(int LingerMs, int MaxInFlight) : Batching;
     }
 }
